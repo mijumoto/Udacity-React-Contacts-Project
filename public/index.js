d85814b92eac566e3b977ed927ec0aeb00de680b
mijumoto <15346105+mijumoto@users.noreply.github.com>
2020-02-01 13:10:42 -0500

    clean start

--- public/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-
-const element = React.createElement('div', null, 'hello world')
-
-ReactDom.render(
-  element,
-  document.getElementById('root')
-)
